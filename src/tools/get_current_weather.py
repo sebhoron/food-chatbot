1645edc131c5aa7f509e85255b2b054696eae0f8
sebhoron <92864754+sebhoron@users.noreply.github.com>
2024-08-08 14:11:41 +0100

    Tidy up

---
+++ src/tools/get_current_weather.py
@@ -0,0 +1,15 @@
+WEATHER_INFO = {
+    "Berlin": {"weather": "mostly sunny", "temperature": 7, "unit": "celsius"},
+    "Paris": {"weather": "mostly cloudy", "temperature": 8, "unit": "celsius"},
+    "Rome": {"weather": "sunny", "temperature": 14, "unit": "celsius"},
+    "Madrid": {"weather": "sunny", "temperature": 10, "unit": "celsius"},
+    "London": {"weather": "cloudy", "temperature": 9, "unit": "celsius"},
+}
+
+
+def get_current_weather(location: str):
+    if location in WEATHER_INFO:
+        return WEATHER_INFO[location]
+
+    else:
+        return {"weather": "sunny", "temperature": 21.8, "unit": "fahrenheit"}
