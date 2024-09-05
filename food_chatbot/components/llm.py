cf0eb19a39460923ec255572e5689ce56a190df4
sebhoron <92864754+sebhoron@users.noreply.github.com>
2024-09-05 17:21:42 +0100

    lint

---
+++ food_chatbot/components/llm.py
@@ -0,0 +1,13 @@
+"""LLM config file"""
+
+import os
+
+from dotenv import load_dotenv
+from haystack.components.generators import AzureOpenAIGenerator
+
+load_dotenv()
+
+llm = AzureOpenAIGenerator(
+    azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+)
